b70dc8160420b721d43db8e372b6e3b088926669
Cristina Simoncini <crisimonc@gmail.com>
2018-11-04 15:25:26 +0100

    added methods to motorcycle and booking controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,43 @@
 class BookingsController < ApplicationController
+
+	def index
+    	@bookings = Booking.user_bookings(current_user)
+  	end
+
+  	def show
+   		@booking = Booking.find(params[:id])
+  	end
+
+  	def new
+    	@motorcycle = Motorcycle.find(params[:motorcycle_id])
+    	@booking = Booking.new
+
+    	@booking_map = Booking.where.not(latitude: nil, longitude: nil)
+
+    	@markers = @booking_map.map do |location|
+      		{
+        		lat: location.latitude,
+        		lng: location.longitude,
+      		}
+    	end
+  	end
+
+  	def create
+    	@booking = Booking.new(booking_params)
+		@booking.motorcycle = Motorcycle.find(params[:motorcycle_id])
+    	@booking.user = current_user
+
+    	if @booking.save
+      		redirect_to motorcycle_booking_path(@booking.motorcycle, @booking)
+    	else
+      		render :new
+    	end
+  	end
+
+
+private
+
+ 	 def booking_params
+    	params.require(:booking).permit(:start_date, :end_date, :motorcycle_id, :user_id, :address)
+  	end
 end
